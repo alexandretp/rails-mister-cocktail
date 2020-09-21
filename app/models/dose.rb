d6cd55bdcc5ec2c5978d84f0ef7362270719ff2b
Alexandre Pereira <alexandretenorio01@gmail.com>
2020-09-21 15:34:53 -0300

    Prepared to put on Heroku

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
-
   validates :ingredient_id, :cocktail_id, :description, presence: true
   validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
 end
